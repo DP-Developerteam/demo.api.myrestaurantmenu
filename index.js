88f75b3e4c294664a01667e2b7645a7d6ed289e7
Diego Pérez Rodríguez <diego@MacBook-Pro-de-Diego.local>
2025-04-29 22:28:43 +0200

    use session cookie domain vercel

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use(session({
         secure: true, // Set false for local test
         // sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         sameSite: 'none',
-        domain: '.dp-app-restaurant-menu-demo.vercel.app',
+        domain: '.vercel.app',
         httpOnly: true, // Prevent client-side JS cookie access
         maxAge: 4 * 60 * 60 * 1000, // 4 hour session duration
         path: '/'
